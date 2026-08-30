83ed748f23491735ae525deef820476c81760445
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-30 21:01:31 +0530

    Run brand icon repair on public portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import PortfolioReference from './PortfolioReference';
 import DynamicPortfolioStats from './DynamicPortfolioStats';
 import TechnologyToolsPortal from './TechnologyToolsPortal';
 import TechSizeTuning from './TechSizeTuning';
+import BrandIconRepair from './BrandIconRepair';
 import JarvisAssistant from './JarvisAssistant';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/><TechSizeTuning/><JarvisAssistant/></>}
+export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/><TechSizeTuning/><BrandIconRepair/><JarvisAssistant/></>}
