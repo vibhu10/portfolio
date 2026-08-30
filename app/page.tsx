c35de7b10079dfbb5afa6ea74ad4b7ea9aefe9db
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-30 21:13:37 +0530

    Place redesigned stack in original page flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import PortfolioReference from './PortfolioReference';
 import DynamicPortfolioStats from './DynamicPortfolioStats';
-import TechnologyToolsShowcase from './TechnologyToolsShowcase';
+import TechnologyToolsMount from './TechnologyToolsMount';
 import JarvisAssistant from './JarvisAssistant';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsShowcase/><JarvisAssistant/></>}
+export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsMount/><JarvisAssistant/></>}
