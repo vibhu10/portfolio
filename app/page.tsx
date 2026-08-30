dfe4ac7c50f1718b6bbe155c857693f08c86a3f5
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-30 21:04:47 +0530

    Mount side-by-side technology and tools layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import PortfolioReference from './PortfolioReference';
 import DynamicPortfolioStats from './DynamicPortfolioStats';
 import TechnologyToolsPortal from './TechnologyToolsPortal';
 import TechSizeTuning from './TechSizeTuning';
+import TechColumnsLayout from './TechColumnsLayout';
 import BrandIconRepair from './BrandIconRepair';
 import JarvisAssistant from './JarvisAssistant';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/><TechSizeTuning/><BrandIconRepair/><JarvisAssistant/></>}
+export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/><TechSizeTuning/><TechColumnsLayout/><BrandIconRepair/><JarvisAssistant/></>}
