c02eb928831d31fd6b6357b67a5a995e63d221db
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-30 21:12:18 +0530

    Use redesigned AI technology and tools showcase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import PortfolioReference from './PortfolioReference';
 import DynamicPortfolioStats from './DynamicPortfolioStats';
-import TechnologyToolsPortal from './TechnologyToolsPortal';
-import TechSizeTuning from './TechSizeTuning';
-import TechColumnsLayout from './TechColumnsLayout';
-import BrandIconRepair from './BrandIconRepair';
+import TechnologyToolsShowcase from './TechnologyToolsShowcase';
 import JarvisAssistant from './JarvisAssistant';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/><TechSizeTuning/><TechColumnsLayout/><BrandIconRepair/><JarvisAssistant/></>}
+export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsShowcase/><JarvisAssistant/></>}
