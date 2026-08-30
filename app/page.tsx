51c5ee93eb30c8519f52a11a4f0d1b980f1741de
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-30 21:00:24 +0530

    Apply compact tech card sizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import PortfolioReference from './PortfolioReference';
 import DynamicPortfolioStats from './DynamicPortfolioStats';
 import TechnologyToolsPortal from './TechnologyToolsPortal';
+import TechSizeTuning from './TechSizeTuning';
 import JarvisAssistant from './JarvisAssistant';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/><JarvisAssistant/></>}
+export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/><TechSizeTuning/><JarvisAssistant/></>}
