935587cb6f1e5e2bb4de7eba7e9681cfb78b2557
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-30 20:02:00 +0530

    Render separated technology and tools section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import PortfolioReference from './PortfolioReference';
 import DynamicPortfolioStats from './DynamicPortfolioStats';
+import TechnologyToolsPortal from './TechnologyToolsPortal';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/></>}
+export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsPortal/></>}
