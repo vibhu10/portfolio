7c2d455729ca6c627479b64398dd230f6ad4cbed
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-31 16:14:12 +0530

    Mount public layout and plasma polish layer

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,6 @@ import PortfolioReference from './PortfolioReference';
 import DynamicPortfolioStats from './DynamicPortfolioStats';
 import TechnologyToolsMount from './TechnologyToolsMount';
 import JarvisAssistant from './JarvisAssistant';
+import PublicExperiencePolish from './PublicExperiencePolish';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsMount/><JarvisAssistant/></>}
+export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsMount/><JarvisAssistant/><PublicExperiencePolish/></>}
