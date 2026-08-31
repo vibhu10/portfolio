6ff4265e5b04bf00a5c7a2e46682e84b4d049890
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-31 16:29:23 +0530

    Mount neural Jarvis and live theme runtime

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,7 @@ import DynamicPortfolioStats from './DynamicPortfolioStats';
 import TechnologyToolsMount from './TechnologyToolsMount';
 import JarvisAssistant from './JarvisAssistant';
 import PublicExperiencePolish from './PublicExperiencePolish';
+import ThemeRuntime from './ThemeRuntime';
+import NeuralJarvisSkin from './NeuralJarvisSkin';
 
-export default function Home(){return <><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsMount/><JarvisAssistant/><PublicExperiencePolish/></>}
+export default function Home(){return <><ThemeRuntime/><PortfolioReference/><DynamicPortfolioStats/><TechnologyToolsMount/><JarvisAssistant/><PublicExperiencePolish/><NeuralJarvisSkin/></>}
