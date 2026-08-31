63ad1e35d284656e3d9859f84bc64f88083dfa8b
vibhu10 <145326642+vibhu10@users.noreply.github.com>
2026-08-31 16:29:30 +0530

    Add dual theme controls to admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,5 +2,6 @@ import './cms-panels.css';
 import JarvisAdminLauncher from './JarvisAdminLauncher';
 import BrandIconRepair from '../BrandIconRepair';
 import AdminSessionGuardian from './AdminSessionGuardian';
+import ThemeManager from './ThemeManager';
 
-export default function AdminLayout({children}:{children:React.ReactNode}){return <>{children}<AdminSessionGuardian/><BrandIconRepair/><JarvisAdminLauncher/></>}
+export default function AdminLayout({children}:{children:React.ReactNode}){return <>{children}<AdminSessionGuardian/><BrandIconRepair/><ThemeManager/><JarvisAdminLauncher/></>}
